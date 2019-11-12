97bde70e424c08b4cbb18987cf7d69126b400352
1658608470 <1658608470@qq.com>
2019-11-12 20:13:56 +0800

    Update bad_apple.py
    解决开机运行不显示或没声音

--- Bad Apple/bad_apple.py
+++ Bad Apple/bad_apple.py
@@ -1,10 +1,25 @@
 import video,time
 from Maix import GPIO
+import lcd
+
+lcd.init(freq=15000000)
+
+########### settings ############
+WIFI_EN_PIN     = 8
+# AUDIO_PA_EN_PIN = None  # Bit Dock and old MaixGo
+AUDIO_PA_EN_PIN = 32      # Maix Go(version 2.20)
+# AUDIO_PA_EN_PIN = 2     # Maixduino
 
 # disable wifi
-fm.register(8,  fm.fpioa.GPIO0)
+fm.register(WIFI_EN_PIN, fm.fpioa.GPIO0)
 wifi_en=GPIO(GPIO.GPIO0, GPIO.OUT)
 wifi_en.value(0)
+
+# open audio PA
+if AUDIO_PA_EN_PIN:
+    fm.register(AUDIO_PA_EN_PIN, fm.fpioa.GPIO1)
+    wifi_en=GPIO(GPIO.GPIO1, GPIO.OUT)
+    wifi_en.value(1)
 
 # register i2s(i2s0) pin
 fm.register(34,  fm.fpioa.I2S0_OUT_D1)
