c7ea6eac77806dfdcfeeacaded72efab42e3ee1f
1658608470 <1658608470@qq.com>
2019-11-12 20:22:42 +0800

    Update bad_apple.py
    这样才可以重启自动播放，不再卡在黑屏了

--- Bad Apple/bad_apple.py
+++ Bad Apple/bad_apple.py
@@ -1,6 +1,7 @@
 import video,time
-from Maix import GPIO
 import lcd
+from fpioa_manager import *
+from Maix import GPIO
 
 lcd.init(freq=15000000)
 
@@ -9,6 +10,7 @@ WIFI_EN_PIN     = 8
 # AUDIO_PA_EN_PIN = None  # Bit Dock and old MaixGo
 AUDIO_PA_EN_PIN = 32      # Maix Go(version 2.20)
 # AUDIO_PA_EN_PIN = 2     # Maixduino
+
 
 # disable wifi
 fm.register(WIFI_EN_PIN, fm.fpioa.GPIO0)
@@ -22,13 +24,13 @@ if AUDIO_PA_EN_PIN:
     wifi_en.value(1)
 
 # register i2s(i2s0) pin
-fm.register(34,  fm.fpioa.I2S0_OUT_D1)
-fm.register(35,  fm.fpioa.I2S0_SCLK)
-fm.register(33,  fm.fpioa.I2S0_WS)
+fm.register(34,fm.fpioa.I2S0_OUT_D1)
+fm.register(35,fm.fpioa.I2S0_SCLK)
+fm.register(33,fm.fpioa.I2S0_WS)
 
 v = video.open("/sd/bad_apple.avi")
 print(v)
-v.volume(20)
+v.volume(15)
 while True:
     if v.play() == 0:
         print("play end")
